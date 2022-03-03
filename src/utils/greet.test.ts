6c86b1d326ef8aae3f8066068774e98b49a8121b
matbat10 <97300316+matbatten@users.noreply.github.com>
2022-03-03 08:17:18 +0000

    changes to name

--- src/utils/greet.test.ts
+++ src/utils/greet.test.ts
@@ -2,6 +2,6 @@ import { greet } from "./greet";
 
 test("greet returns a string, greeting the passed name", () => {
   expect(greet("World")).toBe("Hello, World!");
-  expect(greet("Richard")).toBe("Hello, Richard!");
+  expect(greet("Mat")).toBe("Hello, Mat!");
   expect(greet("Academy Scholars")).toBe("Hello, Academy Scholars!");
 });
